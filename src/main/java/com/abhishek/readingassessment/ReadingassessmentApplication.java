e06e0010b13bfbacefa8380387cb005c0192697c
101010watson <abhisheksahu22999@gmail.com>
2026-07-12 22:14:47 +0530

    adding the maven dependencies

---
+++ src/main/java/com/abhishek/readingassessment/ReadingassessmentApplication.java
@@ -0,0 +1,13 @@
+package com.abhishek.readingassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingassessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingassessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abhishek/readingassessment/ReadingassessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishek.readingassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingassessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
