8af48462a7eb723e6ff51cda536558a3c66e6857
101010watson <abhisheksahu22999@gmail.com>
2026-07-18 18:11:18 +0530

    add comments, update generate endpoint
    added comments throughout the page for understandability
    updated generate enpoint to return error if no content is pasted or
    uploaded.

--- src/main/java/com/abhishek/readingassessment/controller/HomeController.java
+++ src/main/java/com/abhishek/readingassessment/controller/HomeController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
 import java.util.*;
@@ -32,38 +33,59 @@ public class HomeController {
          return "default-page";
      }
 
+     // (required = false) means that if user uploads pdf and leaves text box empty
+     // "sourceText is optional. If it's not sent, don't throw an error."
+     // and Similarly for the pdf File too
     @PostMapping("/generate")
-    public String generate(@RequestParam(required = false) String sourceText, @RequestParam(required = false) MultipartFile pdfFile, Model model, HttpSession httpSession) throws IOException {
-        // recieves the reponse object
+    public String generate(@RequestParam(required = false) String sourceText,
+                           @RequestParam(required = false) MultipartFile pdfFile,
+                           Model model, HttpSession httpSession,
+                           RedirectAttributes redirectAttributes){
+
+        boolean hasPdf = pdfFile != null && !pdfFile.isEmpty();
+        boolean hasText = sourceText != null && !sourceText.trim().isEmpty();
+
+        // if user does not paste or upload any content
+        if(!hasPdf && !hasText){
+            redirectAttributes.addFlashAttribute(
+                    "error","Please paste some text or upload a PDF."
+            );
+            return "redirect:/";
+        }
+
         try{
-            if (!pdfFile.isEmpty()) {
+            if (hasPdf) {
                 sourceText = pdfService.extractText(pdfFile);
             }
+            // receives the response object
             QuizResponseDTO quizResponseDTO = aiService.generateMcq(sourceText);
-            httpSession.setAttribute("quiz", quizResponseDTO);
+            httpSession.setAttribute("quiz", quizResponseDTO); // stores the response in the session for later use
             model.addAttribute("quiz", quizResponseDTO);
             return "prep-page";
         } catch (Exception e) {
-            model.addAttribute("error","unable to generate the quiz. Please check your internt connection");
-            return "default-page";
+            redirectAttributes.addFlashAttribute(
+                    "error",
+                    "Unable to generate the quiz. Please check your internet connection"
+            );
+            return "redirect:/";
         }
      }
 
-     @PostMapping("/submit")
+    @PostMapping("/submit")
     public String submit(
             @RequestParam Map<String, String> answers,Model model,HttpSession httpSession
      ){
         QuizResponseDTO quizResponseDTO = (QuizResponseDTO) httpSession.getAttribute("quiz");
         if(quizResponseDTO == null){
-            return "redirect:/quickprep";
+            return "redirect:/";
         }
         int correct = 0;
         int incorrect = 0;
-        List<QuestionDTO> question = quizResponseDTO.getQuestions();
+        List<QuestionDTO> question = quizResponseDTO.getQuestions(); // retrieve the questions in form of List
         for(int i = 0;i < quizResponseDTO.getQuestions().size();i++){
-            int selectedAnswer = Integer.parseInt(answers.get("question"+i));
+            int selectedAnswer = Integer.parseInt(answers.get("question"+i)); // answer by user
             QuestionDTO currentQuestion = question.get(i);
-            currentQuestion.setSelectedAnswer(selectedAnswer);
+            currentQuestion.setSelectedAnswer(selectedAnswer); // store the selected answer for later comparision
             if(selectedAnswer == currentQuestion.getCorrectAnswer()){
                 correct++;
             }
@@ -84,5 +106,11 @@ public class HomeController {
 }
 
 // @RequestParam - this basically helps to get the content(in our case text) to the java spring boot variable
-// HttpSession - we are using httpsession to save the context so that in the controller we can use the methods
+
+// HttpSession - we are using HttpSession to save the context so that in the controller we can use the methods
 // to get the inputs like here we want to get correctAnswers etc
+
+// --> addFlashAttribute()
+// Flash attributes are specifically designed for this situation(confirm resubmission after an error is displayed).
+// Spring temporarily stores the attribute (internally using the session),
+// then automatically copies it into the model of the next request only.
